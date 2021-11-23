f6b1a67663462b2046c59d8ed7b3036b2ed5ff47
pedro <pedro_cesar93@hotmail.com>
2021-11-23 00:22:19 -0300

    chore: fix conflicts

---
+++ resources/views/menu/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@extends('navbardashboard')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Adicionar Itens</div>
+                <div class="card-body">
+                    qtd de mesas
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Editar Itens</div>
+                <div class="card-body">
+                    qtd de pedidos
+                </div>
+            </div>
+        </div>
+    </div>
